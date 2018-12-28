cb4815f2022238f858ee868ea65957c1fbb0ffef
Birol Işık <birol.isik@hotmail.com.tr>
2018-12-28 16:08:25 +0300

    index.js change some properties

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const render = Component =>
     </Provider>,
     document.getElementById('root'),
   );
+//test
 
 if (process.env.NODE_ENV === 'development') {
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
